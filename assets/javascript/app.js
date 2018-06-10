6476ed7ecc39f78b46bf00281f43d78b02e102e2
BlaiseAK <blaisekress@yahoo.com>
2018-06-10 15:57:53 -0400

    Was able to get start button to show questions upon clicking and made some more css page adjustments

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+$("#start").on("click", function() {
+    $(".startScreenMain").hide();
+    $("#game").show();
+    // start timer function here
+})
+
+// timer countdown function
+
+// counting right and wrong answers here
+
+// when timer = 0 then show results page
