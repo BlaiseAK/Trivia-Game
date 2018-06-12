770b2e3d168dae58f167cc747804f28e5872968b
BlaiseAK <blaisekress@yahoo.com>
2018-06-11 22:10:06 -0400

    The idea of using if/else statements was correct but they needed to be nested in a function to be called and executed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,7 @@
-var time = 30;
-var numberOfCheckedRadio = $(":radio[name='question1']:checked, :radio[name='question2']:checked, :radio[name='question3']:checked, :radio[name='question4']:checked, :radio[name='question5']:checked, :radio[name='question6']:checked, :radio[name='question7']:checked, :radio[name='question8']:checked").length;
+var time = 10;
 var correct = 0;
 var incorrect = 0;
-var unanswered = 8-numberOfCheckedRadio;
+var unanswered = 8;
 
 
 $("#start").on("click", function() {
@@ -12,22 +11,6 @@ $("#start").on("click", function() {
     start();
 })
 
-// counting right and wrong answers here
-
-
-// when timer = 0 then show results page
-function results () {
-    $("#game").hide();
-    $("#results").show();
-    $("#results").html("<h4>You answered " + correct + " correct</h4><br><h4>You answered " + incorrect + " incorrect</h4><br><h4>You did not answer "+ unanswered + "</h4>")
-    
-}
-
-// set interval that everysecond run the time
-function intervalCountDown() {
-    $("#timer").html("<h4>You have " + time + " seconds left</h4>");
-    time--;
-}
 // setTimeout(results, 90*1000);
 function start() {
     intervalId = setTimeout (results, time*1000)
@@ -40,24 +23,93 @@ function start() {
     }
 }
 
+// set interval that everysecond run the time
+function intervalCountDown() {
+    $("#timer").html("<h4>You have " + time + " seconds left</h4>");
+    time--;
+}
+
+// when timer = 0 then show results page
+function results () {
+    questionCount();
+    $("#game").hide();
+    $("#results").show();
+    $("#results").html("<h4>You answered " + correct + " correct</h4><br><h4>You answered " + incorrect + " incorrect</h4><br><h4>You did not answer "+ unanswered + "</h4>")
+    
+}
+
+$("input[name='question1']:checked").on("click", function() {
+    console.log($("input[name='question1']:checked").val())
+
+})
+
 var answer1 = $("input[name='question1']:checked").val();
 console.log(answer1)
-// function questionCount () {
-// if question 1 is selected unanswered - 1
-
-// if question 1 input value = correct then correct + 1
-
-// if ($("input[name='question1']:checked").val()="correct") {
-//     correct++;
-//     unanswered--;
-// } else if ($("input[name='question1']:checked").val()="incorrect") {
-//     incorrect++;
-//     unanswered--;
-// }
-
-// else is question 1 input value = incorrect + 1
 
 
+function questionCount () {
 
+if ($( "input[name='question1']:checked" ).val() === "correct") {
+    correct++;
+    unanswered--;
+} else if ($( "input[name='question1']:checked" ).val() === "incorrect") {
+    incorrect++;
+    unanswered--;
+};
 
-// }
+if ($( "input[name='question2']:checked" ).val() === "correct") {
+    correct++;
+    unanswered--;
+} else if ($( "input[name='question2']:checked" ).val() === "incorrect") {
+    incorrect++;
+    unanswered--;
+};
+
+if ($( "input[name='question3']:checked" ).val() === "correct") {
+    correct++;
+    unanswered--;
+} else if ($( "input[name='question3']:checked" ).val() === "incorrect") {
+    incorrect++;
+    unanswered--;
+};
+
+if ($( "input[name='question4']:checked" ).val() === "correct") {
+    correct++;
+    unanswered--;
+} else if ($( "input[name='question4']:checked" ).val() === "incorrect") {
+    incorrect++;
+    unanswered--;
+};
+
+if ($( "input[name='question5']:checked" ).val() === "correct") {
+    correct++;
+    unanswered--;
+} else if ($( "input[name='question5']:checked" ).val() === "incorrect") {
+    incorrect++;
+    unanswered--;
+};
+
+if ($( "input[name='question6']:checked" ).val() === "correct") {
+    correct++;
+    unanswered--;
+} else if ($( "input[name='question6']:checked" ).val() === "incorrect") {
+    incorrect++;
+    unanswered--;
+};
+
+if ($( "input[name='question7']:checked" ).val() === "correct") {
+    correct++;
+    unanswered--;
+} else if ($( "input[name='question7']:checked" ).val() === "incorrect") {
+    incorrect++;
+    unanswered--;
+};
+
+if ($( "input[name='question8']:checked" ).val() === "correct") {
+    correct++;
+    unanswered--;
+} else if ($( "input[name='question8']:checked" ).val() === "incorrect") {
+    incorrect++;
+    unanswered--;
+};
+}
